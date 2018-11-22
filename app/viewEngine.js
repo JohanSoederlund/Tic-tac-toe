f5984c1b144f3ffad4cc367eb08b9da04d2c6db5
Johan Söderlund <js223zs@student.lnu.se>
2018-11-22 15:59:42 +0100

    REFACTOR console output for viewEngine

--- app/viewEngine.js
+++ app/viewEngine.js
@@ -6,17 +6,26 @@
  * V2.0 Web based.
  */
 export default class ViewEngine {
+
 	constructor() {
 
     }
     
     renderStartGame() {
+        console.log("Tic-tac-toe\n\n Aim for three in a row and win this simple game.\n\n player 1 input name:");
     }
     
     renderGameBoard() {
+        console.log("   {0}  |  {1}  |  {2}   ");
+        console.log("   -------------------");
+        console.log("   {3}  |  {4}  |  {5}   ");
+        console.log("   -------------------");
+        console.log("   {6}  |  {7}  |  {8}   ");
+                    
     }
 
     renderEndGame() {
+        console.log(" players: this.players\n roundNumber: this.roundNumber\n winner: this.winner");
     }
 
 }
