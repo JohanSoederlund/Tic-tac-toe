275f25639c67e2181f7d1832b7c48db710700df3
Johan Söderlund <js223zs@student.lnu.se>
2018-11-23 14:37:02 +0100

    REFACTOR restore connectionSpy

--- test/database-manager-test-suite.js
+++ test/database-manager-test-suite.js
@@ -60,6 +60,7 @@ export function run() {
 				});
 
                 after(() => {
+                    connectionSpy2.restore();
                     mongoose.connection.close();
 				});
                 
